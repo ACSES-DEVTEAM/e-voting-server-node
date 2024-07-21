dfdcbc2ba81fe7642d00903e7c8fecdd3fddf0a7
Bankah Anthony Bekoe <154749514+bankah@users.noreply.github.com>
2024-07-21 15:20:28 +0000

    Modified voteController

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -10,45 +10,47 @@ const getVotes = async (req, res) => {
 };
 
 // Add vote
- const addVote = async (req, res) => {
-   const { indexNumbers } = req.body;    // array of indexNumber of candidates that user voted
- 
-   const { id } = req.params;           // id of user
-   if (!mongoose.Types.ObjectId.isValid(id)) {
-     return res.status(404).json({ error: "No such user" });
-   }
- 
-  const candidates = await Candidate.find({ indexNumber: { $in: indexNumbers } });
- 
+const addVote = async (req, res) => {
+  const { indexNumbers } = req.body; // array of indexNumber of candidates that user voted
+
+  const { id } = req.params; // id of user
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such user" });
+  }
+
+  const candidates = await Candidate.find({
+    indexNumber: { $in: indexNumbers },
+  });
+
   if (candidates.length !== indexNumbers.length) {
-     return res.status(404).json({ error: "No such candidate from candidate" });
-   }
- 
-   // check if user has already voted
+    return res.status(404).json({ error: "No such candidate from candidate" });
+  }
+
+  // check if user has already voted
   const vote = await Vote.findOne({ voters_id: id });
   if (vote) {
-     return res.status(400).json({ error: "User has already voted" });
-   }
- 
-   // add the id and candidate's indexNumber to vote
+    return res.status(400).json({ error: "User has already voted" });
+  }
+
+  // add the id and candidate's indexNumber to vote
   const newVotes = {
-     voters_id: id,
-    candiate_indexNumber: indexNumbers
+    voters_id: id,
+    candiate_indexNumber: indexNumbers,
   };
   await Vote.create(newVotes);
- 
-   // update candidate's vote with vote from req.body
+
+  // update candidate's vote with vote from req.body
   const updatedCandidates = await Candidate.updateMany(
     { indexNumber: { $in: indexNumbers } },
-     { $inc: { votes: 1 } },
-     { new: true }
-   );
+    { $inc: { votes: 1 } },
+    { new: true }
+  );
   res.status(200).json(updatedCandidates);
- };
+};
 
 // Reset Vote by indexNumber
 const resetVote = async (req, res) => {
-  const { indexNumber } = req.body;                                   // array of indexNumber of candidates that user reset vote
+  const { indexNumber } = req.body; // array of indexNumber of candidates that user reset vote
 
   const candidate = await Candidate.findOne({ indexNumber });
 
